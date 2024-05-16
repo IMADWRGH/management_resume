7c0f599886ecb744afcf01f6e52236f3bba2268c
Imad Asserne <imad.asserne@gmail.com>
2024-05-16 10:53:30 +0000

    first commit

---
+++ src/main/java/IMADWRGH/Gestion_CV/Repository/ResumeCompanyRepository.java
@@ -0,0 +1,25 @@
+package IMADWRGH.Gestion_CV.Repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ResumeCompanyRepository {
+    private final JdbcTemplate template;
+
+    public ResumeCompanyRepository(JdbcTemplate template) {
+        this.template = template;
+    }
+
+    public int deleteById(int id) {
+        return template.update("delete from resume_company where id=?", new Object[] {id});
+    }
+
+    public int insert(int idResume, int idCompany) {
+        String idResumeStr = String.valueOf(idResume);
+        String idCompanyStr = String.valueOf(idCompany);
+        return template.update("insert into resume_company (id_resume, id_company) VALUES (?,?)",
+                idResumeStr, idCompanyStr);
+    }
+
+}
