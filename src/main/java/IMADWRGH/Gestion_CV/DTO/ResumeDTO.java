22e1391c13f21cd95cc7d5a1d1b31041594b099a
Imad Asserne <imad.asserne@gmail.com>
2024-05-17 09:33:50 +0000

    first commit

---
+++ src/main/java/IMADWRGH/Gestion_CV/DTO/ResumeDTO.java
@@ -0,0 +1,14 @@
+package IMADWRGH.Gestion_CV.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResumeDTO {
+    private  int id;
+    private String profile;
+    private int idInfons;
+}
