40795c0ba06c5bbf943e25e247f0410130700648
Imad Asserne <imad.asserne@gmail.com>
2024-05-20 14:34:03 +0000

    first commit

---
+++ src/main/java/IMADWRGH/Gestion_CV/Exception/ResumeNotFoundException.java
@@ -0,0 +1,8 @@
+package IMADWRGH.Gestion_CV.Exception;
+
+public class ResumeNotFoundException extends RuntimeException{
+    public ResumeNotFoundException(String msg){
+        super(msg);
+    }
+
+}
